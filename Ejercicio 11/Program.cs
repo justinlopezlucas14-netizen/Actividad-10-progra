1a8ce815ab247c254bc889191e9cd43d69206d33
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 16:23:43 -0600

    Eliminacion de -

---
+++ Ejercicio 11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> estudiantes = new Dictionary<string, int> {
+            {"Juan", 20}, {"Maria", 22}, {"Pedro", 19}, {"Lucia", 21}, {"Elena", 23}
+        };
+
+        foreach (var item in estudiantes)
+        {
+            Console.WriteLine($"Nombre: {item.Key} - Edad: {item.Value}");
+        }
+    }
+}
---
+++ Ejercicio 12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> productos = new Dictionary<int, string>
+        {
+            { 501, "Laptop Dell" },
+            { 502, "Mouse Inalámbrico" },
+            { 503, "Teclado Mecánico" },
+            { 504, "Monitor 27'" }
+        };
+
+        Console.WriteLine("Lista de Productos Almacenados");
+
+        foreach (KeyValuePair<int, string> producto in productos)
+        {
+            Console.WriteLine($"Código: {producto.Key} Nombre: {producto.Value}");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
