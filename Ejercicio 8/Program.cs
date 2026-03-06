5ff8bcc7565a319f5225d15c9203d1bdac50304e
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 16:12:42 -0600

    Cambio de notas

---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> empleados = new Dictionary<int, string>
+        {
+            { 201, "Pedro Armas" },
+            { 202, "Luisa Cano" },
+            { 203, "Roberto Sosa" }
+        };
+
+        Console.WriteLine("Antes de la modificación");
+        foreach (var emp in empleados) Console.WriteLine($"ID: {emp.Key} - Nombre: {emp.Value}");
+
+        int idAModificar = 202;
+        string nuevoNombre = "Luisa Cano de Pérez";
+
+
+        empleados[idAModificar] = nuevoNombre;
+
+        Console.WriteLine($"\nDespués de modificar al ID {idAModificar} ");
+        foreach (var emp in empleados) Console.WriteLine($"ID: {emp.Key} - Nombre: {emp.Value}");
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, double> registroNotas = new Dictionary<string, double>
+        {
+            { "Carlos", 85 },
+            { "Elena", 92 },
+            { "Mario", 74 },
+            { "Adriana", 100 }
+        };
+
+        Console.WriteLine("Sistema de Consulta de Notas");
+        Console.Write("Ingrese el nombre del estudiante a consultar: ");
+        string nombreInput = Console.ReadLine();
+
+        if (registroNotas.ContainsKey(nombreInput))
+        {
+            double nota = registroNotas[nombreInput];
+            Console.WriteLine($"\nLa nota de {nombreInput} es: {nota}");
+        }
+        else
+        {
+            Console.WriteLine($"\nError: El estudiante '{nombreInput}' no se encuentra registrado.");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
