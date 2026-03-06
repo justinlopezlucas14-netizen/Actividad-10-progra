0433d3378955ec2ea36efc7d696927001e553614
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 15:41:01 -0600

    Cambio de codigos

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> ciudades = new Dictionary<int, string> {
+            {502, "Guatemala"},
+            {34, "España"},
+            {57, "Colombia"},
+            {52, "México"},
+            {51, "Perú"}
+        };
+
+        Console.WriteLine("Buscador de Ciudades por Código");
+        Console.Write("Por favor, ingrese el código de área: ");
+
+        if (int.TryParse(Console.ReadLine(), out int codigo))
+        {
+            if (ciudades.ContainsKey(codigo))
+            {
+                string pais = ciudades[codigo];
+                Console.WriteLine($"\nResultado: El código {codigo} pertenece a {pais}.");
+            }
+            else
+            {
+                Console.WriteLine($"\nLo sentimos, el código {codigo} no se encuentra en nuestra base de datos.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nError: Debe ingresar un número válido.");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
