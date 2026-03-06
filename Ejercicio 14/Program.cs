71f5433e9c3554343262830750209696d95f23a1
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 16:34:50 -0600

    Cambios en el codigo

---
+++ Ejercicio 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> ciudades = new Dictionary<string, string>
+        {
+            { "GUA", "Guatemala" },
+            { "MEX", "México" },
+            { "ESP", "España" },
+            { "ARG", "Argentina" }
+        };
+
+        Console.WriteLine("Visualización de Datos Separados ");
+
+        Console.Write("Claves del diccionario: ");
+        foreach (string clave in ciudades.Keys)
+        {
+            Console.Write($"[{clave}] ");
+        }
+
+        Console.Write("Valores del diccionario: ");
+        foreach (string valor in ciudades.Values)
+        {
+            Console.Write($"{valor}, ");
+        }
+
+        Console.WriteLine("\n\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
