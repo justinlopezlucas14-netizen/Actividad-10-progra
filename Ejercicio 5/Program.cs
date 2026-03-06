ddb07e5e60d0c64995ae5c4e17c5f3d2148e92c4
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 15:44:48 -0600

    cambio de usuario y contraseña

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> cuentas = new Dictionary<string, string>
+        {
+            { "admin", "password123" },
+            { "juan_perez", "guatemala2024" },
+            { "maria_g", "flores99" },
+            { "usuario_invitado", "invitado" }
+        };
+
+        Console.WriteLine("Buscador de Credenciales");
+        Console.Write("Ingrese el nombre de usuario que desea buscar: ");
+        string usuarioABuscar = Console.ReadLine();
+
+        if (cuentas.ContainsKey(usuarioABuscar))
+        {
+            string pass = cuentas[usuarioABuscar];
+            Console.WriteLine($"\nUsuario encontrado: {usuarioABuscar}");
+            Console.WriteLine($"Contraseña: {pass}");
+        }
+        else
+        {
+            Console.WriteLine("\nError: El usuario ingresado no existe en el sistema.");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
