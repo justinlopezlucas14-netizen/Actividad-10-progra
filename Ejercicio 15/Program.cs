b4e0f0446bb00502cf6467ee6829dea4f15fcf0e
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 16:40:46 -0600

    Correccion de programa

---
+++ Ejercicio 15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> empleados = new Dictionary<int, string>
+        {
+            { 101, "Carlos Pérez" },
+            { 102, "Ana García" },
+            { 103, "Roberto Gómez" },
+            { 104, "Lucía Méndez" }
+        };
+
+        Console.WriteLine("Listado de Empleados con Formato");
+
+        foreach (var emp in empleados)
+        {
+            Console.WriteLine($"ID: {emp.Key}  Nombre: {emp.Value}");
+        }
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
