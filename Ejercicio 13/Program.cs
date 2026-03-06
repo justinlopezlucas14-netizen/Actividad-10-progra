cfbd424f51255fa5fa7c64746c671974bb1e2b4c
Adri4 <justinlopezlucas14@gmail.com>
2026-03-06 16:28:50 -0600

    Agregue un estudiante mas y compuse las notas

---
+++ Ejercicio 13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, double> notas = new Dictionary<string, double>
+        {
+            { "Juan", 85 },
+            { "Maria", 92 },
+            { "Pedro", 78 },
+            { "Ana", 88},
+            {"Luis",90},
+        };
+
+        double sumaTotal = 0;
+
+        foreach (double nota in notas.Values)
+        {
+            sumaTotal += nota;
+        }
+
+        double promedio = sumaTotal / notas.Count;
+
+        Console.WriteLine("Cálculo de Promedio");
+        Console.WriteLine($"Total de estudiantes: {notas.Count}");
+        Console.WriteLine($"Suma de notas: {sumaTotal}");
+        Console.WriteLine($"El promedio general es: {promedio:F2}");
+
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
